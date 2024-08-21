9952fd97da0898b2a54dfba46e8292ac65f7bcc6
meghna237 <meghucrajesh@gmail.com>
2024-08-21 14:26:25 +0530

    edits for github page

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import { BrowserRouter as Route, Routes, HashRouter } from 'react-router-dom';
 import Flashcard from './components/Flashcard';
 import Subjects from './components/Subject';
 import Login from './components/Login';
@@ -9,14 +9,14 @@ import { UserProvider } from './context/UserContext'; // Import UserProvider
 function App() {
   return (
     <UserProvider>
-      <Router>
+      <HashRouter>
         <Routes>
           <Route exact path="/" element={<Login />} />
           <Route path="/signup" element={<SignUp />} />
           <Route path="/subjects" element={<Subjects />} />
           <Route path="/questions" element={<Flashcard />} />
         </Routes>
-      </Router>
+      </HashRouter>
     </UserProvider>
   );
 }
